72bb51359817f57a0b9898bead0349bba0c75716
jaggr2 <jaggr2@bfh.ch>
2013-12-20 17:32:06 +0100

    StrategieManual improvements

--- src/Source/CStrategieManual.java
+++ src/Source/CStrategieManual.java
@@ -4,6 +4,7 @@ import Util.CPosition;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.HashMap;
 
 /**
  * User: bohnp1
@@ -13,7 +14,16 @@ import java.awt.event.KeyListener;
 public class CStrategieManual implements CStrategie, KeyListener {
 
     private Double angle = 0.0;
-    private Double stepSize = 3.0;
+    private Double stepSize = 0.0;
+
+    private Boolean isRightPressed = false;
+    private Boolean isLeftPressed = false;
+    private Boolean isUpPressed = false;
+    private Boolean isDownPressed = false;
+    private Boolean isShiftPressed = false;
+
+    public static final Double NORMAL_SPEED = 3.0;
+    public static final Double HIGH_SPEED = 6.0;
 
     /**
      * calculates the next position and saves the result to the nextDesiredPosition member var
@@ -49,6 +59,67 @@ public class CStrategieManual implements CStrategie, KeyListener {
         return walker.getPosition();
     }
 
+    public void recalcValues(KeyEvent e, Boolean isPressed) {
+
+        switch(e.getKeyCode()) {
+            case KeyEvent.VK_RIGHT:
+                isRightPressed = isPressed;
+                e.consume();
+                break;
+            case KeyEvent.VK_LEFT:
+                isLeftPressed = isPressed;
+                e.consume();
+                break;
+            case KeyEvent.VK_UP:
+                isUpPressed = isPressed;
+                e.consume();
+                break;
+            case KeyEvent.VK_DOWN:
+                isDownPressed = isPressed;
+                e.consume();
+                break;
+            case KeyEvent.VK_SHIFT:
+                isShiftPressed = isPressed;
+                e.consume();
+                break;
+        }
+
+        if(e.isConsumed()) {
+
+            if(isDownPressed || isUpPressed || isLeftPressed || isRightPressed) {
+                this.stepSize = ( isShiftPressed ? HIGH_SPEED : NORMAL_SPEED );
+            }
+            else {
+                this.stepSize = 0.0;
+            }
+
+            if(isUpPressed && isRightPressed) {
+                this.angle = Math.PI * 7/4;
+            }
+            else if(isUpPressed && isLeftPressed) {
+                this.angle = Math.PI * 5/4;
+            }
+            else if(isDownPressed && isRightPressed) {
+                this.angle = Math.PI * 1/4;
+            }
+            else if(isDownPressed && isLeftPressed) {
+                this.angle = Math.PI * 3/4;
+            }
+            else if(isRightPressed) {
+                this.angle = 0.0;
+            }
+            else if(isLeftPressed) {
+                this.angle = Math.PI;
+            }
+            else if(isUpPressed) {
+                this.angle = Math.PI * 3/2;
+            }
+            else if(isDownPressed) {
+                this.angle = Math.PI / 2;
+            }
+        }
+    }
+
     @Override
     public void keyTyped(KeyEvent e) {
 
@@ -56,28 +127,11 @@ public class CStrategieManual implements CStrategie, KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        switch(e.getKeyCode()) {
-            case KeyEvent.VK_RIGHT:
-                this.angle = 0.0;
-                e.consume();
-                break;
-            case KeyEvent.VK_LEFT:
-                this.angle = Math.PI;
-                e.consume();
-                break;
-            case KeyEvent.VK_UP:
-                this.angle = Math.PI * 3/2;
-                e.consume();
-                break;
-            case KeyEvent.VK_DOWN:
-                this.angle = Math.PI / 2;
-                e.consume();
-                break;
-        }
+        recalcValues(e, true);
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-
+        recalcValues(e, false);
     }
 }
