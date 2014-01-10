5ef49a601defddd9f9fb98a70f2fa4ff636b3404
vonop1 <vonop1@bfh.ch>
2014-01-10 14:45:57 +0100

    comments for class

--- src/GUI/CEditorWalker.java
+++ src/GUI/CEditorWalker.java
@@ -11,17 +11,32 @@ import Util.CPosition;
  */
 public class CEditorWalker extends CWalker {
 
+    //number of walkers on a start position
     private int count;
 
+    /**
+     * constructor for new editor walkers
+     * @param start the start position
+     * @param target the end position
+     * @param count the number of walkers on the position
+     */
     public CEditorWalker(CPosition start, CPosition target, int count) {
         super(start, target, null);
         this.count = count;
     }
 
+    /**
+     * getter for count
+     * @return numbers of walkers
+     */
     public int getCount(){
         return this.count;
     }
 
+    /**
+     * setter for count
+     * @param count new numbers of walker
+     */
     public void setCount(int count){
         this.count = count;
     }
